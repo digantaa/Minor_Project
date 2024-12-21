f41ff94ab2f27381c7b049a3e7ae99de7d107444
Diganta Singh Panwar <114343344+digantaa@users.noreply.github.com>
2024-12-21 20:37:49 +0530

    initial commmit

---
+++ backend/config/db.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+dotenv.config();
+const colors = require("colors");
+
+
+console.log(process.env.MONGO_URI);
+
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+
+    console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline);
+  } catch (error) {
+    console.error(`Error: ${error.message}`.red.bold);
+    process.exit(1); // Exit with a non-zero status code to indicate an error
+  }
+};
+
+module.exports = connectDB;
+
